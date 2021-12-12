b8e356ce031bdff1769eb1c31873b34750abd708
leonardo809 <67874944+leonardo809@users.noreply.github.com>
2021-12-12 13:18:20 -0600

    Add files via upload

--- VGamesReview/DataBase/UserDB.cs
+++ VGamesReview/DataBase/UserDB.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace VGamesReview.DataBase
 {
-    public class UserDB
+   
+    public class UserDB : DBConnection
     {
+        public int usua;
+        public bool LoginU(string email, string pass) {
+            OpenConnetion();
+            User user;
+            string query = "select * from USERS where password_user=@pass and EMAIL_USER=@email";
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand(query, cnx);
+                sqlCommand.Parameters.AddWithValue("@pass", pass);
+                sqlCommand.Parameters.AddWithValue("@email",email);
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+               user = new User();
+                while (dataReader.Read())
+                {
+                   user = new User(Int16.Parse(dataReader.GetValue(0).ToString()), dataReader.GetValue(1).ToString(), dataReader.GetValue(2).ToString(), dataReader.GetValue(3).ToString());
+                    System.Diagnostics.Debug.WriteLine(Int16.Parse(dataReader.GetValue(0).ToString()) + dataReader.GetValue(1).ToString() + dataReader.GetValue(2).ToString() + dataReader.GetValue(3).ToString() + dataReader.GetValue(4).ToString());
+                }
+                if (user.NameUser != null )
+                {
+                    usua = user.IdUser;
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                user = new User();
+            }
+            return false;
+        }
     }
 }
