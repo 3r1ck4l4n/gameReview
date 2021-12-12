537fc10c4148bad90f8bdfc304210442af61c04c
leonardo809 <67874944+leonardo809@users.noreply.github.com>
2021-12-12 13:16:51 -0600

    Add files via upload

--- VGamesReview/Login.aspx.cs
+++ VGamesReview/Login.aspx.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VGamesReview.DataBase;
 
 namespace VGamesReview
 {
+   
+
     public partial class WebForm2 : System.Web.UI.Page
     {
+     
         protected void Page_Load(object sender, EventArgs e)
         {
+           
+        }
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            
+            UserDB userDB = new UserDB();
+            UserDB u = userDB;
+        
+            if (u.LoginU(email.Value.ToString(), Password1.Value.ToString()))
+            {
+                
+                Session["userLogged"] = u.usua;
+                email.Value = Session["userLogged"].ToString();
+             Response.Redirect("AllGAmes.aspx");
+            }
+
 
         }
     }
